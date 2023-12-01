de5e13b91fd5cb1abd52260f13a17cbd6a54a83a
RogerAgu <bobjackson298@gmail.com>
2023-11-30 20:49:48 -0600

    edited

--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -35,6 +35,41 @@
 <template>
   <div class="card" @click="selectCard">
     <div class="card-image">
+      <img :src="faker.internet.avatar()" alt="" srcset="" />
+    </div>
+    <div class="card-details">
+      <p class="card-details-name">{{ props.employee.name }}</p>
+      <p class="card-details-job">{{ props.employee.title }}, {{ department.name }}</p>
+      <p class="card-details-quote">"{{ props.employee.quote }}"</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+  .card {
+    @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-2xl hover:shadow-slate-900;
+    &-image {
+      img {
+        @apply mx-auto rounded-full object-contain;
+      }
+    }
+    &-details {
+      @apply flex flex-col gap-2  pt-6 text-center;
+      &-name {
+        @apply text-3xl font-thin  tracking-wider text-slate-800;
+      }
+      &-job {
+        @apply -mt-2 text-xs font-bold text-yellow-700;
+      }
+      &-quote {
+        @apply pt-4 text-lg italic text-slate-800;
+      }
+    }
+  }
+</style>
+<template>
+  <div class="card" @click="selectCard">
+    <div class="card-image">
       <img :src="faker.image.url()" alt="" srcset="" />
     </div>
     <div class="card-details">
